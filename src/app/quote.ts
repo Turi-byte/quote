9d89d1519ad60c3fad4b7fa8508c938b9bbc9a28
Turi-byte <breldanmuturi001@gmail.com>
2019-08-16 16:27:48 +0300

    edit the quote-detail.component.html

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,11 @@
 export class Quote {
   showDetails: boolean;
-  constructor(public id: number, public name: string, public author: string, public publisher: string,public completeDate:Date , public upvotes: number,public downvotes: number){
+  upvotes: number;
+  downvotes: number;
+  constructor(public id: number, public name: string, public author: string, public publisher: string,public completeDate:Date){
       this.showDetails=false;
+      this.upvotes = 0;
+      this.downvotes = 0;
   }
 }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Never,never give up.', 'Winston Churchill', 'Turi',new Date(2019,0,9),0,0),
-    new Quote(2, 'Teach me how to dougie up in this.', 'Soulja Boy','Lucy',new Date(2019,0,11),0,0),
-    new Quote(3, 'A poor workman quarrels with his tools', 'Winchester men', 'Steve',new Date(2019,2,9),0,0),
-    new Quote(4, 'Pride comes before a fall', 'Marcus Riley', 'Marcus',new Date(2019,9,9),0,0),
-    new Quote(5, 'Whatever it takes', 'Timmy Tdat', 'Jeff',new Date(2019,8,11),0,0),
-    new Quote(6, 'One mans trash is another man\'s treasure', 'Mr.Anonymous', 'Kibe',new Date(2019,21,10),0,0),
+    new Quote(1, 'Never,never give up.', 'Winston Churchill', 'Turi',new Date(2019,0,9)),
+    new Quote(2, 'Teach me how to dougie up in this.', 'Soulja Boy','Lucy',new Date(2019,0,11)),
+    new Quote(3, 'A poor workman quarrels with his tools', 'Winchester men', 'Steve',new Date(2019,2,9)),
+    new Quote(4, 'Pride comes before a fall', 'Marcus Riley', 'Marcus',new Date(2019,9,9)),
+    new Quote(5, 'Whatever it takes', 'Timmy Tdat', 'Jeff',new Date(2019,8,11)),
+    new Quote(6, 'One mans trash is another man\'s treasure', 'Mr.Anonymous', 'Kibe',new Date(2019,21,10)),
   ];
 
   toggleDetails(index){
@@ -22,7 +22,7 @@ export class QuoteComponent implements OnInit {
   }
 
   deleteQuote(isDelete,index){
-    let toDelete = confirm(`Are you sure you wanna delete this?`)
+    let toDelete = confirm(`Are you sure you wanna delete ${this.quotes[index].publisher}'s quote?`)
     if (toDelete){
       this.quotes.splice(index,1);
     }

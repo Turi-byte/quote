d6757bb0b1b5a333333d13a196e9c1cc3fa065fa
Turi-byte <breldanmuturi001@gmail.com>
2019-08-16 15:27:41 +0300

    Add the quote date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDetails: boolean;
-  constructor(public id: number, public name: string, public author: string, public publisher: string, public upvotes: number,public downvotes: number){
+  constructor(public id: number, public name: string, public author: string, public publisher: string,public completeDate:Date , public upvotes: number,public downvotes: number){
       this.showDetails=false;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Never,never give up.', 'Winston Churchill', 'Turi',0,0),
-    new Quote(2, 'Teach me how to dougie up in this.', 'Soulja Boy', 'Lucy',0,0),
-    new Quote(3, 'A poor workman quarrels with his tools', 'Winchester men', 'Steve',0,0),
-    new Quote(4, 'Pride comes before a fall', 'Marcus Riley', 'Marcus',0,0),
-    new Quote(5, 'Whatever it takes', 'Timmy Tdat', 'Jeff',0,0),
-    new Quote(6, 'One mans trash is another man\'s treasure', 'Mr.Anonymous', 'Kibe',0,0),
+    new Quote(1, 'Never,never give up.', 'Winston Churchill', 'Turi',new Date(2019,0,9),0,0),
+    new Quote(2, 'Teach me how to dougie up in this.', 'Soulja Boy','Lucy',new Date(2019,0,11),0,0),
+    new Quote(3, 'A poor workman quarrels with his tools', 'Winchester men', 'Steve',new Date(2019,2,9),0,0),
+    new Quote(4, 'Pride comes before a fall', 'Marcus Riley', 'Marcus',new Date(2019,9,9),0,0),
+    new Quote(5, 'Whatever it takes', 'Timmy Tdat', 'Jeff',new Date(2019,8,11),0,0),
+    new Quote(6, 'One mans trash is another man\'s treasure', 'Mr.Anonymous', 'Kibe',new Date(2019,21,10),0,0),
   ];
 
   toggleDetails(index){

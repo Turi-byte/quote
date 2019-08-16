56530e621e157fdb4f6c1a3e9c86a3a1cb4399c2
Turi-byte <breldanmuturi001@gmail.com>
2019-08-16 11:08:19 +0300

    create a class for quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'; 
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- quotes: string [];
- constructor(){
-   this.quotes =['Never,never give up','Don\'t lose faith','Teach my how to dougie']
- }
+ quotes: Quote[]= [
+  {id:1,name:'Never,never give up.',author:'Winston Churchill'},
+  {id:2,name:'Teach me how to dougie up in this.',author:'Soulja Boy'},
+  {id:3,name:'A poor workman quarrels with his tools',author:'Winchester men'},
+  {id:4,name:'Pride comes before a fall',author:'Marcus Riley'},
+  {id:5,name:'Whatever it takes',author:'Timmy Tdat'},
+  {id:6,name:'One mans trash is another man\'s treasure',author:'Mr.Anonymous'},
+ ];
 }

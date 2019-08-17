03fd517fbe75a8142e1abe84a5209896a0e18bc9
Turi-byte <breldanmuturi001@gmail.com>
2019-08-17 14:22:06 +0300

    Add a functional form for adding quotes

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,10 +8,10 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number{
     let today:Date = new Date();//get current date and time
     let todayWithNoTime: any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
-    var dateDifference = Math.floor(todayWithNoTime-value)//returns value in milliseconds
+    var dateDifference = Math.abs(todayWithNoTime-value)//returns value in milliseconds
     const secondsInDay = 86400; // 60 seconds * 60 minutes * 24 hours
     var dateDifferenceSeconds = dateDifference*0.001;//converts milliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    var dateCounter = Math.floor(dateDifferenceSeconds/secondsInDay);
 
     if (dateCounter >= 1 && value < todayWithNoTime){
       return dateCounter;

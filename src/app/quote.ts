abed78df9f3767245e1d08a96e59a3e58df109e3
Turi-byte <breldanmuturi001@gmail.com>
2019-08-18 15:52:50 +0300

    Polish the portfolio state

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,8 @@
 export class Quote {
   public showDetails: boolean;
   public upvotes: number;
-  public downvotes: number;
-  constructor(public id: number, public name: string, public author: string, public publisher: string, public completeDate: Date) {
+  public downvotes: number;  
+  constructor(public id: number, public name: string, public author: string, public publisher: string,public completeDate: Date) {
     this.showDetails = false;
     this.upvotes = 0;
     this.downvotes = 0;
